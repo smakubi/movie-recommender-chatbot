5dd0a901ad87a43024d4a8dd78b45d44274dc066
smakubi <smakubi>
2024-10-22 02:08:02 +0000

    azure compute

--- resources/compute.py
+++ resources/compute.py
@@ -9,9 +9,8 @@ URL = f"{API_URL}/api/2.0/clusters/create"
 
 
 CLUSTER_CONFIG = {
-    "num_workers": 0,
     "cluster_name": f"{USER_NAME}'s LLM Cluster",
-    "spark_version": "15.2.x-cpu-ml-scala2.12",
+    "spark_version": "15.4.x-cpu-ml-scala2.12",
     "spark_conf": {
         "spark.master": "local[*, 4]",
         "spark.databricks.cluster.profile": "singleNode"
@@ -26,13 +25,12 @@ CLUSTER_CONFIG = {
     "custom_tags": {
         "ResourceClass": "SingleNode"
     },
-    "spark_env_vars": {},
     "autotermination_minutes": 120,
-    "init_scripts": [],
+    "enable_elastic_disk": True,
     "single_user_name": f"{EMAIL}",
-    "enable_local_disk_encryption": False,
     "data_security_mode": "SINGLE_USER",
-    "runtime_engine": "STANDARD"
+    "runtime_engine": "STANDARD",
+    "num_workers": 0,
 }
 
 response = requests.post(URL, json=CLUSTER_CONFIG, headers={"Authorization": f"Bearer {TOKEN}"})
